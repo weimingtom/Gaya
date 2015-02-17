072eb37858e6af39d4de3017721825092b39d507
Ernestas Mitkus <ernestas@inventi.lt>
2015-02-17 22:31:27 +0200

    Change company name

---
+++ core/src/com/ernestas/gaya/Util/Settings.java
@@ -0,0 +1,32 @@
+package com.ernestas.gaya.Util;
+
+public class Settings {
+
+    private int width;
+    private int height;
+
+    private static Settings instance = null;
+
+    private Settings() {}
+
+    public static Settings getInstance() {
+        if (instance == null) {
+            instance = new Settings();
+        }
+        return instance;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
---
+++ desktop/src/com/ernestas/gaya/desktop/DesktopLauncher.java
@@ -0,0 +1,27 @@
+package com.ernestas.gaya.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gaya.gaya.GayaEntry;
+import com.gaya.gaya.Util.Settings;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+        double scale = 1.2;
+        Settings.getInstance().setWidth((int) (320 * scale));
+        Settings.getInstance().setHeight((int) (480 * scale));
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.title = "Gaya pre-alpha v0.0.2";
+        config.width = Settings.getInstance().getWidth();
+        config.height = Settings.getInstance().getHeight();
+        config.resizable = false;
+//        config.useGL30 = true; // Use GL30 for performance improvement
+
+        config.foregroundFPS = 2;
+        config.backgroundFPS = 2;
+
+        new LwjglApplication(new GayaEntry(), config);
+	}
+}
